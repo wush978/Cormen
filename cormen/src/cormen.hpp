cf71b9b4571b0e01ee71d8767551186fb6db9a81
Wush Wu <wush978@gmail.com>
2012-07-03 00:22:37 +0800

    finish matrix-multiplication

---
+++ cormen/src/cormen.hpp
@@ -0,0 +1,15 @@
+/*
+ * cormen.hpp
+ *
+ *  Created on: Jul 2, 2012
+ *      Author: wush
+ */
+
+#ifndef CORMEN_HPP_
+#define CORMEN_HPP_
+
+#include <Rcpp.h>
+
+RcppExport SEXP MatrixMultiplication(SEXP dim_list);
+
+#endif /* CORMEN_HPP_ */
---
+++ cormen/tests/matrix-multiplication.R
@@ -0,0 +1,2 @@
+dyn.load("../cormen/libs/cormen.so")
+.Call("MatrixMultiplication",c(30, 35, 15, 5, 10, 20, 25))
